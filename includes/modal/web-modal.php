28918fcc76bbdcffe44734561cc2e6e530c579b7
SalmanSheikh30272 <salman.hafeez30272@gmail.com>
2024-06-27 03:11:21 -0700

    Modal Done

--- includes/modal/web-modal.php
+++ includes/modal/web-modal.php
@@ -1,58 +1,52 @@
 <!-- PopUp Modal Start Here -->
 <div class="modal fade" id="staticBackdrop" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" fdprocessedid="0122n">
-                        <i class="fa fa-times" aria-hidden="true"></i>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-xxl-7 col-xl-7 col-lg-7 col-md-7 col-sm-12 col-12 p-0">
-                            <div class="modal-body-form">
-                                <h3>Save Upto 50% <span>Limited Time Offer</span></h3>
-                                <form action="https://leads.infinityprojectmanager.com/brand/wikithinkers/lead" method="GET">
-                                    <input type="hidden" name="brand_name" value="wikithinkers.com">
-                                    <input type="hidden" id="lead_area" name="lead_area" value="http://www.wikithinkers.com">
-                                    <div class="popup-form-list">
-                                        <div class="form-group">
-                                            <input type="text" name="name" placeholder="Your Name" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="email" name="email" placeholder="Your Email" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="number" name="phone" placeholder="Phone Number" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <textarea id="message" name="message" placeholder="Your Message" required></textarea>
-                                        </div>
-                                        <div class="form-group form-group-last">
-                                            <button class="submit-btn" type="submit">Submit</button>
-                                        </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" fdprocessedid="0122n">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="modal-body-form">
+                            <h3>Let's Get <span>Started</span></h3>
+                            <form action="https://leads.infinityprojectmanager.com/brand/wikinnovators/lead" method="GET">
+                                <input name="brand_name" type="hidden" value="wikinnovators.com">
+                                <input id="lead_area" name="modal_form" type="hidden" value="https://wikinnovators.com/">
+                                <div class="popup-form-list">
+                                    <div class="form-group">
+                                        <input type="text" name="name" placeholder="Your Name" required>
                                     </div>
-                                </form>
-                            </div>
+                                    <div class="form-group">
+                                        <input type="email" name="email" placeholder="Your Email" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="number" name="phone" placeholder="Phone Number" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea id="message" name="message" placeholder="Your Message" required></textarea>
+                                    </div>
+                                    <div class="form-group form-group-last">
+                                        <button class="submit-btn" type="submit">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div class="col-xxl-5 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-12 p-0">
-                            <div class="modal-body-img1">
-                            <img src="assets/images/wikipedia-browse-img.webp" data-src="assets/images/wikipedia-browse-img.webp" alt="Wikipedia Browse" width="100%" height="auto"    class="img-fluid">
-                            </div>
-                        </div>
-                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 <!-- PopUp Modal End Here -->
 
 <style>
 /* PopUp Modal Css Start Here */
 .modaL_order{}
 #staticBackdrop .modal-dialog{
-    max-width: 700px !important;
+    max-width: 550px !important;
     margin: 150px auto 0;
 }
 #staticBackdrop .modal-content{
@@ -64,16 +58,15 @@
 }
 #staticBackdrop .modal-content .btn-close{
     position: absolute;
-    right: -110px;
-    left: 0;
+    right: -10px;
     margin: 0 auto;
     top: -5px;
-    background: #FCC127;
+    background: #FD5240;
     font-size: 20px;
     width: 20px;
     height: 20px;
     line-height: 20px;
-    border: 2px solid #FCC127;
+    border: #fff;
     border-radius: 100%;
     outline: none;
     box-shadow: none;
@@ -82,7 +75,7 @@
     opacity: 1;
 }
 #staticBackdrop .modal-content .btn-close i{
-    color: #212121;
+    color: #fff;
     font-weight: 600;
 }
 #staticBackdrop .modal-header{
@@ -92,45 +85,25 @@
 #staticBackdrop .modal-body{
     padding: 0;
 }
-#staticBackdrop .modal-body .modal-body-img1{
-    width: 100%;
-    height: 100%;
-    position: relative;
-    z-index: 1;
-    display: flex;
-    align-items: end;
-    justify-content: center;
-}
-#staticBackdrop .modal-body .modal-body-img1::after{
-    content: '';
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-color: #fbc25a;
-    height: 200px;
-    width: 100%;
-    z-index: -1;
-}
-#staticBackdrop .modal-body .modal-body-img1 img{
-    width: 100%;
-    margin: 0 0 30px 0;
-    animation: Rotate 7s linear infinite;
-}
 #staticBackdrop .modal-body .modal-body-form{
-    background: #FBFBFB;
+    background-image: url(assets/images/bg-img/service-offer-bg.png);
+    background-color: #080731;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
     padding: 30px 30px;
-    border-radius: 50px 50px 0px 50px
+    border-radius: 5px;
+    text-align: center;
 }
 .modal-body-form h3{
     font-size: 30px;
     line-height: 36px;
-    color: #212121;
-    margin: 0 0 10px;
+    color: #fff;
+    margin: 0 0 20px;
 }
 .modal-body-form h3 span{
-    color: #fbc25a;
-    display: block;
+    font-weight: 600;
+    color: #FD5240;
 }
 .modal-body-form .popup-form-list{}
 .modal-body-form .popup-form-list .form-group{
@@ -142,54 +115,55 @@
 }
 .modal-body-form .popup-form-list .form-group input{
     background: #fff;
+    color: #141414;
     font-size: 16px;
-    color: #212121;
-    padding: 10px 20px;
     width: 100%;
-    height: 60px;
     border: none;
+    border-radius: 0px;
     outline: none;
-    border-radius: 50px;
+    padding: 15px 20px;
 }
 .modal-body-form .popup-form-list .form-group input::placeholder{
-    color: #A4A4A4;
+    color: #b2b2b2;
 }
 .modal-body-form .popup-form-list .form-group textarea{
     background: #fff;
+    color: #141414;
     font-size: 16px;
-    color: #212121;
     width: 100%;
     height: 120px;
-    resize: none;
     border: none;
-    border-radius: 30px;
-    padding: 20px 20px;
+    border-radius: 0px;
+    resize: none;
+    outline: none;
+    padding: 10px 20px;
+    box-shadow: none;
 }
 .modal-body-form .popup-form-list .form-group textarea::placeholder{
-    color: #A4A4A4;
+    color: #b2b2b2;
 }
 .modal-body-form .popup-form-list .form-group button.submit-btn{
-    font-family: "Calibri-Bold";
-    background: linear-gradient(to right, #212121 50%, #FCC127 50%);
-    background-size: 200% 100%;
-    background-position: right bottom;
-    border: 2px solid #FCC127;
-    color: #212121;
-    font-size: 18px;
-    width: 100%;
-    height: 60px;
-    border-radius: 50px;
-    padding: 8px 10px;
+    background: #FD5240;
+    color: #fff;
+    border: 2px solid #FD5240;
+    font-size: 16px;
+    font-weight: 600;
+    width: 175px;
+    height: 50px;
+    border-radius: 0px;
+    padding: 5px 5px;
+    margin: 0 auto;
     display: flex;
     align-items: center;
     justify-content: center;
-    transition: all .3s ease-in-out;
     text-transform: capitalize;
+    transition: all .3s ease-in-out;
+    position: relative;
 }
 .modal-body-form .popup-form-list .form-group button.submit-btn:hover{
-    background-position: left bottom;
-    color: #fff;
-    border: 2px solid #212121;
+    background: #fff;
+    color: #FD5240;
+    transform: scale(1.1);
 }
 .modal.show{
     padding-right:0px !important;
@@ -207,12 +181,6 @@
     left: inherit;
     margin: 0;
     top: -5px;
-}
-#staticBackdrop .modal-body .modal-body-img1{
-    display: none;
-}
-#staticBackdrop .modal-body .modal-body-form{
-    border-radius: 50px 50px;
 }
 .modal-body-form h3{
     font-size: 26px;
@@ -244,12 +212,6 @@
     margin: 0;
     top: -5px;
 }
-#staticBackdrop .modal-body .modal-body-img1{
-    display: none;
-}
-#staticBackdrop .modal-body .modal-body-form{
-    border-radius: 50px 50px;
-}
 .modal-body-form h3{
     font-size: 26px;
     line-height: 32px;
@@ -266,3 +228,4 @@
 /* PopUp Modal Responsive Css End Here */
 }
 </style>
+<!-- PopUp Modal End Here -->
